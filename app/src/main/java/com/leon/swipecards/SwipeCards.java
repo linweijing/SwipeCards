08f1306a5311e63fdabbe806d69b13df2a392416
Leon <526247082@qq.com>
2017-06-07 23:49:25 +0800

    Add ViewDragHelper

--- app/src/main/java/com/leon/swipecards/SwipeCards.java
+++ app/src/main/java/com/leon/swipecards/SwipeCards.java
@@ -1,7 +1,10 @@
 package com.leon.swipecards;
 
 import android.content.Context;
+import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,12 +17,17 @@ public class SwipeCards extends ViewGroup {
     private int mCenterX;
     private int mCenterY;
 
+    private ViewDragHelper mViewDragHelper;
+
+    private int mCardGap = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, getResources().getDisplayMetrics());
+
     public SwipeCards(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public SwipeCards(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mViewDragHelper = ViewDragHelper.create(this, mCallback);
     }
 
     @Override
@@ -39,10 +47,33 @@ public class SwipeCards extends ViewGroup {
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
             int left = mCenterX - child.getMeasuredWidth() / 2;
-            int top = mCenterY - child.getMeasuredHeight() / 2;
+            int top = mCenterY - child.getMeasuredHeight() / 2 + mCardGap * (getChildCount() - i);
             int right = left + child.getMeasuredWidth();
             int bottom = top + child.getMeasuredHeight();
             child.layout(left, top, right, bottom);
         }
+    }
+
+    private ViewDragHelper.Callback mCallback = new ViewDragHelper.Callback() {
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return true;
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return left;
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return top;
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mViewDragHelper.processTouchEvent(event);
+        return true;
     }
 }
